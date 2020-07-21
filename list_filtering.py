6ece3a09357fbc639ce782d4cb5afa423685e671
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-07-21 15:59:39 -0500

    Add list filtering example

--- list_filtering.py
+++ list_filtering.py
@@ -1,12 +1,16 @@
 # In this kata you will create a function that takes a list of non-negative integers and strings 
 # and returns a new list with the strings filtered out.
 
-def filter_list(l):
-  for element in l:
-    if Number(.isnumeric() and element >= 0:
-      filtered[filtered.size()] = element
-  return filtered
-          
-filter_list([1,2,'a','b']) == [1,2]
-filter_list([1,'a','b',0,15]) == [1,0,15]
-filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+def filter_list(data_list):
+  index = 0
+  returnValue = []
+  for elem in data_list:
+   
+    try:
+      returnValue.append(int(elem))
+      index += 1
+
+    except ValueError:
+      pass
+ 
+  return returnValue
---
+++ list_filtering_test.py
@@ -0,0 +1,9 @@
+import unittest
+from list_filtering import filter_list
+
+class Test_List_Filtering(unittest.TestCase):
+    
+    def test_list_filtering(self):
+        self.assertEqual(filter_list([1,2,'a','b']),[1,2])
+        self.assertEqual(filter_list([1,'a','b',0,15]),[1,0,15])
+        self.assertEqual(filter_list([1,2,'aasf',123]),[1,2,123])
